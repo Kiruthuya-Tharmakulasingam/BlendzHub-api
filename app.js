f9d0f4f45c2a95ecdaf6b247d12cabffbb6c2e88
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-11-03 21:24:16 +0530

    base routescreated

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import customerRoutes from "./routes/customerRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+import equipmentRoutes from "./routes/equipmentRoutes.js";
+import feedbackRoutes from "./routes/feedbackRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import serviceRoutes from "./routes/serviceRoutes.js";
+import staffRoutes from "./routes/staffRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Our App");
+});
+
+app.use("/api/customers", customerRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/equipments", equipmentRoutes);
+app.use("/api/feedbacks", feedbackRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/staffs", staffRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
